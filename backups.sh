20105d7dd8e817413f5f6da7a2ac7be30f1e372c
richiprieto <richiprieto@crice.org>
2020-10-05 14:32:09 -0300

    script para backup con cron

---
+++ backups.sh
@@ -0,0 +1,10 @@
+# Creamos carpeta con la fecha del backup
+mkdir /root/backups/$(date +%d-%m-%Y)
+# con mysqldump hacemos el backup de la base de datos
+# la base de datos tendra de nombre la misma fecha del backup
+mysqldump -h <host> -u <user> -C -Q -e --create-options <db> > <dir>/backups/$(date +%d-%m-%Y)/$(date +%d-%m-%Y).sql
+# hacemos backups de carpetas
+cp -R <dir_original> <dir>/backups/$(date +%d-%m-%Y)/
+cp -R <dir_original> <dir>/backups/$(date +%d-%m-%Y)/
+# Borramos alguna carpeta que no deseamos hacer backup
+rm -rf <dir>/backups/$(date +%d-%m-%Y)/<carpeta_eliminar>
